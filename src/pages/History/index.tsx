5ce5b4bf8863e6e2cda556c02f8caf4c3e11b7da
jhameson <jhamesonlucas22@gmail.com>
2023-08-20 17:55:55 -0300

    melhorando a aplicação

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -2,7 +2,7 @@ import { useContext } from 'react'
 import { formatDistanceToNow } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 import { CyclesContext } from '../../contexts/CyclesContext'
-import { HistoryContainer, HistoryList, Status } from './styles'
+import { HistoryContainer, HistoryList, Status } from './style'
 
 export function History() {
   const { cycles } = useContext(CyclesContext)
